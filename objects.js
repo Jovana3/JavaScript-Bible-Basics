9ec90e044e1f4309bdbdcf040c2df86f1b68d08e
veljoj <jovana.veljovic@msg-global.com>
2021-08-11 17:17:35 +0200

    Objects

---
+++ objects.js
@@ -0,0 +1,29 @@
+var a = {
+  x: 1,
+  y: 2,
+};
+
+console.log(a);
+
+a = () => {
+  console.log("JO");
+};
+
+// a();
+console.log(a);
+
+// Object is a single value type -> reference type
+
+// Object is a collection of the name value pairs
+// Object (built in default variable) is a function
+
+// If we assign specific value of object type to the variable, then the variab
+
+// Object is a description of different entities
+// Most often object is a term for variable that holds value of object type
+
+// Default variable in JS called Object - this variable is a function with key-value pairs
+
+// Every function in JS is an object
+
+// Statemants in the console -> Undefined
